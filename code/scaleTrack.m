668801c26176317c5e0029d15e93e53f658a276e
Divya Koyyalagunta <divyakoyyalagunta@Divyas-MacBook-Pro.local>
2017-11-16 07:04:48 -0500

    bug fixes

--- code/scaleTrack.m
+++ code/scaleTrack.m
@@ -1,10 +1,10 @@
 function d = scaleTrack(I, J, xI, sigma)
     pI = gaussianPyramid(I, 0.5);
     pJ = gaussianPyramid(J, 0.5);
-    d = lucas_kanade(pI{4}, pJ{4}, xI/16, sigma);
+    d = lucas_kanade(pI{4}, pJ{4}, xI/8, sigma);
     
     for i = 3:1    
-       d = lucas_kanade(pI{i}, pJ{i}, xI/(2^i), sigma, d*2);    
+       d = lucas_kanade(pI{i}, pJ{i}, xI/(2^(i-1)), sigma, d*2);    
     end
 end
 
